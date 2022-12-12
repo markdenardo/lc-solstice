b8ce895564d112851aae18e2a70f2c8791f3b6cb
MDN <mark.denardo@gmail.com>
2022-12-12 12:22:11 -0500

    Create deck-01.js

---
+++ deck-01.js
@@ -0,0 +1,32 @@
+// solstice 
+// MDN @markdenardo 2022
+
+// disco diffusion
+
+// giphy bank
+	s0.initImage("https://upload.wikimedia.org/wikipedia/en/7/7c/Solstice_Gameplay.jpg")
+	s0.initImage("")
+
+// antechamber
+src(s1).add(s1).out(o0)
+
+//p5 sketch
+var p1 = new P5()
+s1.init({src:p1.canvas})
+//p5 text
+p1.draw=()=>{
+  p1.text('word', p1.mouseX, p1.mouseY);
+}
+
+//p5 primitives
+
+//twine embed
+
+
+//p5 sound ?
+//await import("blabla.xyz/p5sound.js")
+
+//fractal
+
+// reference links
+// https://upload.wikimedia.org/wikipedia/en/7/7c/Solstice_Gameplay.jpg
